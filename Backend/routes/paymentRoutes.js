b5ffc61e139ad18547feb84d59da1b3f3aa5cc60
RhutuSalave <rhutu10salave@gmail.com>
2024-11-15 18:03:57 +0530

    Delete Backend directory
    deleted

--- Backend/routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const paymentController = require('../controllers/paymentController');
-const router = express.Router();
-
-router.post('/process', paymentController.processPayment);
-
-module.exports = router;
